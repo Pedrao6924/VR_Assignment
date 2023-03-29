ef47c504636c8285b0a28d02b8d3c024412c6811
Pedrao6924 <alt.pedrohfa@gmail.com>
2023-03-29 04:12:50 -0300

    vr stuff

--- Assets/Scripts/Turntable/ApplianceManager.cs
+++ Assets/Scripts/Turntable/ApplianceManager.cs
@@ -6,7 +6,7 @@ public class ApplianceManager : MonoBehaviour
 {
     public GameObject appliance;
     public bool isOcupied = false;
-    Vector3 spawnLocation = new Vector3(1.19f, 0.87f, -1.86f);
+    Vector3 spawnLocation = new Vector3(-18.95f,0.5f,27.92f);
 
     
     public void SpawnAppliance(GameObject app){
@@ -14,7 +14,7 @@ public class ApplianceManager : MonoBehaviour
         if(isOcupied){
             Destroy(appliance);
         }
-        GameObject obj = Instantiate(app, spawnLocation, new Quaternion(0,0,0,0));
+        GameObject obj = Instantiate(app, spawnLocation, new Quaternion(0,180,0,0));
         appliance = obj;
         isOcupied=true;
     }
